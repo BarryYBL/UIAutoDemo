812145b3e30f888576b068399f659545dea7c876
yuanbaolei <2652612315@qq.com>
2020-07-27 11:13:50 +0800

    *** 1.添加了常用的浏览器驱动下载链接

--- Browser_Driver/__init__.py
+++ Browser_Driver/__init__.py
@@ -1,4 +1,12 @@
 """
 注意检查 浏览器驱动 是否于本地浏览器版本一致
 Chrome浏览器驱动下载地址：http://npm.taobao.org/mirrors/chromedriver/
+
+geckodriver(Firefox):https://github.com/mozilla/geckodriver/releases
+
+IEDriverServer(IE):http://selenium-release.storage.googleapis.com/index.html
+
+operadriver(Opera):https://github.com/operasoftware/operachromiumdriver/releases
+
+MicrosoftWebDriver(Edge):https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver
 """
